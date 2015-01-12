79213f3b590bf163acd58db7658853cc155b1b38
Peter Heuz <jan-peter.heuzeroth@student.hpi.uni-potsdam.de>
2015-01-12 20:27:37 +0100

    post method fixed

--- todo-api/app.py
+++ todo-api/app.py
@@ -40,7 +40,7 @@ def create_task():
 	if not request.json or not 'title' in request.json:
 		abort(400)
 	task={
-		'id': task[-1]['id'] + 1,
+		'id': taskss[-1]['id'] + 1,
 		'title': request.json['title'],
 		'description': request.json.get('description', ""),
 		'done': False
